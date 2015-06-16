294873263ddb2eb8f1d8714bf913d5c101dd7e1b
shahingut <shahin.gut@gmail.com>
2015-06-16 16:18:57 +0200

    cleanup

--- CvViewerAngular/App/TestCtrl.js
+++ CvViewerAngular/App/TestCtrl.js
@@ -1,4 +1,4 @@
-var myApp = angular.module('myApp', []);
+﻿var myApp = angular.module('myApp', []);
 
 //  Force AngularJS to call our JSON Web Service with a 'GET' rather than an 'OPTION' 
 //  Taken from: http://better-inter.net/enabling-cors-in-angular-js/
@@ -6,53 +6,6 @@ myApp.config(['$httpProvider', function ($httpProvider) {
     $httpProvider.defaults.useXDomain = true;
     delete $httpProvider.defaults.headers.common['X-Requested-With'];
 }]);
-
-myApp.filter('sumByKey', function () {
-    return function (data, key) {
-        if (typeof (data) === 'undefined' || typeof (key) === 'undefined') {
-            return 0;
-        }
-        var sum = 0;
-        for (var i = data.length - 1; i >= 0; i--) {
-            sum += parseInt(data[i][key]);
-        }
-        return sum;
-    };
-})
-
-myApp.filter('customSum', function () {
-    return function (listOfProducts, key) {
-        //  Count how many items are in this order
-        var total = 0;
-        angular.forEach(listOfProducts, function (product) {
-            //            alert(product + "." + key);
-            total += eval("product." + key);
-        });
-        return total;
-    }
-});
-
-myApp.filter('countItemsInOrder', function () {
-    return function (listOfProducts) {
-        //  Count how many items are in this order
-        var total = 0;
-        angular.forEach(listOfProducts, function (product) {
-            total += product.Quantity;
-        });
-        return total;
-    }
-});
-
-myApp.filter('orderTotal', function () {
-    return function (listOfProducts) {
-        //  Calculate the total value of a particular Order
-        var total = 0;
-        angular.forEach(listOfProducts, function (product) {
-            total += product.Quantity * product.UnitPrice;
-        });
-        return total;
-    }
-});
 
 myApp.controller('TestCtrl',
     function ($scope, $http) {

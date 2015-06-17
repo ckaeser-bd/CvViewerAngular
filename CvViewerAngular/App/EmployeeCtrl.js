de8044272e03b4b11c4d4e3a11fcfb73da0e77cb
ckaeser <christian.kaeser@businessdecision.com>
2015-06-17 13:19:43 +0200

    cleanup

--- CvViewerAngular/App/EmployeeCtrl.js
+++ CvViewerAngular/App/EmployeeCtrl.js
@@ -1,59 +1,79 @@
 ﻿var myApp = angular.module('myApp', []);
+//var serviceUrl = 'http://localhost:8080/api/employee/';
+var serviceUrl = 'http://employeewebapi.azurewebsites.net/api/employee/';
 
 //  Force AngularJS to call our JSON Web Service with a 'GET' rather than an 'OPTION' 
 //  Taken from: http://better-inter.net/enabling-cors-in-angular-js/
-myApp.config(['$httpProvider', function ($httpProvider) {
-    $httpProvider.defaults.useXDomain = true;
-    delete $httpProvider.defaults.headers.common['X-Requested-With'];
-}]);
+myApp.config([
+    '$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
 
-myApp.controller('EmployeeCtrl',
-    function ($scope, $http) {
+myApp.controller('EmployeeCtrl', function($scope, $http) {
+    //  here we'll load our list of employees from our JSON Web Service 
+    $scope.listOfEmployees = null;
 
-        //  here we'll load our list of employees from our JSON Web Service 
-        $scope.listOfEmployees = null;
+    //  When the user selects an "Employee" from our MasterView list, we'll set the following variable.
+    $scope.selectedEmployee = null;
 
-        //  When the user selects a "Employee" from our MasterView list, we'll set the following variable.
-        $scope.selectedEmployee = null;
+    $http.get(serviceUrl)
+        .success(function(data) {
+            $scope.listOfEmployees = data;
 
-        $http.get('http://employeewebapi.azurewebsites.net/api/employee')
+            if ($scope.listOfEmployees.length > 0) {
 
-            .success(function (data) {
-                $scope.listOfEmployees = data;
+                //  If we managed to load more than one Employee record, then select the first record by default.
+                //  This line of code also prevents AngularJS from adding a "blank" <option> record in our drop down list
+                $scope.selectedEmployee = $scope.listOfEmployees[0].Id;
+                //  Load the Details
+                $scope.loadDetails();
+            }
+        })
+        .error(function(data, status, headers, config) {
+            $scope.errorMessage = "Couldn't load the list of employees, error # " + status;
+        });
 
-                if ($scope.listOfEmployees.length > 0) {
+    $scope.selectEmployee = function(val) {
+        //  If the user clicks on a <div>, we can get the ng-click to call this function, to set a new selected Employee.
+        $scope.selectedEmployee = val.Id;
+        $scope.loadDetails();
+    }
 
-                    //  If we managed to load more than one Customer record, then select the first record by default.
-                    //  This line of code also prevents AngularJS from adding a "blank" <option> record in our drop down list
-                    //  (to cater for the blank value it'd find in the "selectedCustomer" variable)
-                    $scope.selectedEmployee = $scope.listOfEmployees[0].Id;
+    $scope.loadDetails = function() {
+        //  Reset our list 
+        $scope.listOfDetails = null;
 
-                    //  Load the list of Orders, and their Products, that this Customer has ever made.
-                    $scope.loadDetails();
-                }
+        //  The user has selected an employee
+        $http.get(serviceUrl + $scope.selectedEmployee)
+            //$http.get('http://employeewebapi.azurewebsites.net/api/employee/' + $scope.selectedEmployee)
+            .success(function(data) {
+                $scope.listOfDetails = data;
             })
-            .error(function (data, status, headers, config) {
-                $scope.errorMessage = "Couldn't load the list of employees, error # " + status;
+            .error(function(data, status, headers, config) {
+                $scope.errorMessage = "Couldn't load the list of Details, error # " + status;
             });
+    }
 
-        $scope.selectEmployee = function (val) {
-            //  If the user clicks on a <div>, we can get the ng-click to call this function, to set a new selected Employee.
-            $scope.selectedEmployee = val.Id;
-            $scope.loadDetails();
-        }
+    $scope.sendPut = function() {
+        $http.put(serviceUrl + $scope.selectedEmployee, $scope.listOfDetails)
+            .success(function(data, status) {
+                alert(data + " " + status);
+            });
+    }
 
-        $scope.loadDetails = function () {
-            //  Reset our list of orders  (when binded, this'll ensure the previous list of orders disappears from the screen while we're loading our JSON data)
-            $scope.listOfDetails = null;
+    $scope.sendPost = function () {
+        $http.post(serviceUrl, $scope.listOfDetails)
+            .success(function (data, status) {
+                alert(data + " " + status);
+            });
+    }
 
-            //  The user has selected a Customer from our Drop Down List.  Let's load this Customer's records.
-            $http.get('http://employeewebapi.azurewebsites.net/api/employee/' + $scope.selectedEmployee)
-                    .success(function (data) {
-                        $scope.listOfDetails = data;
-                         
-                    })
-                    .error(function (data, status, headers, config) {
-                        $scope.errorMessage = "Couldn't load the list of Details, error # " + status;
-                    });
-        }
-    });
+    $scope.sendDelete = function () {
+        $http.delete(serviceUrl + $scope.selectedEmployee)
+            .success(function (data, status) {
+                alert(data + " " + status);
+            });
+    }
+});
